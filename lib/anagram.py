8ba85c5464541da9bd4cee3b1e7eb997a988889b
Lawrence-wambugu <wambugulawrence@gmail.com>
2025-05-11 17:43:48 +0300

    pass lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        sorted_word = sorted(self.word)
+        return [
+            candidate for candidate in words
+            if sorted(candidate.lower()) == sorted_word and candidate.lower() != self.word
+        ]
